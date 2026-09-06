73bdf0b970e79792fcec999bdd88bc6299cae035
Vitalij Nykyforenko <vitalij.nykyforenko@pm.me>
2026-09-06 10:35:48 +0300

    update hypr

--- modules/autostart.lua
+++ modules/autostart.lua
@@ -1,3 +1,6 @@
 hl.on("hyprland.start", function()
+	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+	hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
+
 	hl.exec_cmd("qs -c main")
 end)
